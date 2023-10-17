8d7ffa85c27757f39d0d6f44086f3bdba8e33bcd
Max Masse <maxmasse199@gmail.com>
2023-10-17 17:15:23 -0400

    configuration slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -2,21 +2,58 @@ import React from "react";
 import leftChevron from "../../assets/left-arrow.svg";
 import rightChevron from "../../assets/right-arrow.svg";
 import "./slider.scss";
+import { useState } from "react";
+import Data from "../data/sliderData";
 
 export default function Slider() {
+  const [sliderValue, setSliderValue] = useState(3);
+
+  let SliderActions = {
+    handleDecrease: () => {
+      if (sliderValue === 1) {
+        setSliderValue(5);
+      } else {
+        setSliderValue(sliderValue - 1);
+      }
+    },
+
+    handleIncrease: () => {
+      if (sliderValue === 5) {
+        setSliderValue(1);
+      } else {
+        setSliderValue(sliderValue + 1);
+      }
+    },
+  };
+
   return (
     <>
-      <p className="index-info">3/5 </p>
+      <p className="index-info">{sliderValue}/5 </p>
 
       <div className="slider">
-        <p className="img-info">Bedroom</p>
-        <img src="/images/img-3.jpg" alt="" className="slider-img" />
+        {Data.filter((item) => item.id === sliderValue).map((filteredItem) => (
+          <p className="img-info" key={filteredItem.id}>
+            {filteredItem.description}
+          </p>
+        ))}
 
-        <button className="navigation-button prev-button">
+        <img
+          src={`/images/img-${sliderValue}.jpg`}
+          alt=""
+          className="slider-img"
+        />
+
+        <button
+          onClick={SliderActions.handleDecrease}
+          className="navigation-button prev-button"
+        >
           <img src={leftChevron} alt="" />
         </button>
 
-        <button className="navigation-button next-button">
+        <button
+          onClick={SliderActions.handleIncrease}
+          className="navigation-button next-button"
+        >
           <img src={rightChevron} alt="" />
         </button>
       </div>
