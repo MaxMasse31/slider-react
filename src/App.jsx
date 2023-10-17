f2cf737b0f32cbcc20853156ed58fd7811b40dd8
Max Masse <maxmasse199@gmail.com>
2023-10-17 15:21:01 -0400

    add Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Slider from "./Components/Slider/Slider"
+import Slider from "./Components/Slider/Slider";
+import './App.scss'
 
 
 function App() {
 
   return (
-    <>
+    <div className="App">
    <Slider/>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import leftChevron from "../../assets/left-arrow.svg";
 import rightChevron from "../../assets/right-arrow.svg";
+import "./slider.scss";
 
 export default function Slider() {
   return (
@@ -8,14 +9,14 @@ export default function Slider() {
       <p className="index-info">3/5 </p>
 
       <div className="slider">
-        <p className="image-info">Bedroom</p>
+        <p className="img-info">Bedroom</p>
         <img src="/images/img-3.jpg" alt="" className="slider-img" />
 
         <button className="navigation-button prev-button">
           <img src={leftChevron} alt="" />
         </button>
 
-        <button className="navigation-button prev-button">
+        <button className="navigation-button next-button">
           <img src={rightChevron} alt="" />
         </button>
       </div>
